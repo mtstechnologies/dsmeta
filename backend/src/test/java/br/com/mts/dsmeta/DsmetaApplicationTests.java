b8b9a931c912ac84bc4fd2a2982d4743402173ed
mtstechnologies <michael.trindade@hotmail.com>
2022-09-09 11:47:10 -0400

    Project created

---
+++ backend/src/test/java/br/com/mts/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mts.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
